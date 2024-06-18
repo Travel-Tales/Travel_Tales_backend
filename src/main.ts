c155d1bad4118e5a8419591bf9aa9268bc850e77
inde153 <inde153@naver.com>
2024-06-18 17:44:31 +0900

    featur : 예외처리 필터 적용

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,8 @@ async function bootstrap() {
   const PORT = process.env.PORT || 9502;
 
   app.setGlobalPrefix('api');
+
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(PORT);
 }
 bootstrap();
