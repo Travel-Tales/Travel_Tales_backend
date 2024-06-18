01b5cab525acfcf20031e1ef67bcf66be7e07aae
inde153 <inde153@naver.com>
2024-06-18 17:26:38 +0900

    feature : Error filter

---
+++ src/common/exceptions/error.code.ts
@@ -0,0 +1,17 @@
+// src/common/exception/error-code/error.code.ts
+class ErrorCodeVo {
+  readonly status;
+  readonly message;
+
+  constructor(status, message) {
+    this.status = status;
+    this.message = message;
+  }
+}
+
+export type ErrorCode = ErrorCodeVo;
+
+export const NOT_FOUND = new ErrorCodeVo(404, 'Not Found');
+export const INTERNAL_SERVER = new ErrorCodeVo(500, 'Internal Server Error');
+export const FORBIDDEN = new ErrorCodeVo(403, 'Forbidden');
+export const UNAUTHORIZED = new ErrorCodeVo(401, 'Unauthorized');
