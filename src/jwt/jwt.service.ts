8a18cb16aa356a5abe675ff2ec82e1ddb262584f
inde153 <inde153@naver.com>
2024-06-18 22:07:54 +0900

    refactor : 로그인 Payload 생성 JwtService로 메서드 이동

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { IPayload } from './interfaces';
 import * as jwt from 'jsonwebtoken';
+import { User } from 'src/entities';
 
 @Injectable()
 export class JwtService {
-  createAccessToken(payload: IPayload): string {
+  createAccessToken(userInfo: User): string {
+    const payload = this.createPayload(userInfo);
     return jwt.sign(payload, process.env.ACCESS_SECRET_KEY, {
       expiresIn: process.env.ACCESS_SECRET_EXPIRATION,
     });
   }
 
-  createRefreshToken(payload: IPayload): string {
+  createRefreshToken(userInfo: User): string {
+    const payload = this.createPayload(userInfo);
     return jwt.sign(payload, process.env.REFRESH_SECRET_KEY, {
       expiresIn: process.env.REFRESH_SECRET_EXPIRATION,
     });
+  }
+
+  verifyAccessToken(token: string) {}
+
+  private createPayload(userInfo: User): IPayload {
+    return {
+      id: userInfo.id,
+      nickname: userInfo.nickname,
+      email: userInfo.email,
+      loginType: userInfo.loginType,
+    };
   }
 }
