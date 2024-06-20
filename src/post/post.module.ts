81bdfd34904721b0f459a39c6627479927f7e7bf
inde153 <inde153@naver.com>
2024-06-20 15:04:23 +0900

    feat : post module생성 및 컨트롤러 service 구현

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TravelPost } from 'src/entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TravelPost])],
   controllers: [PostController],
   providers: [PostService],
 })
