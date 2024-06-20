e7717d10d0ea40f7fad16dbfd953cc3e71543b6b
inde153 <inde153@naver.com>
2024-06-20 15:04:23 +0900

    feat : post module생성 및 컨트롤러 service 구현

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,11 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
+import { CreateInput, CreateOutPut } from './dtos/create.dto';
 import { PostService } from './post.service';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
-  async createPost() {
-    await this.postService;
+  @Post()
+  async createPost(@Body() createInput: CreateInput): Promise<any> {
+    return this.postService.createPost(createInput);
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TravelPost } from 'src/entities';
+import { Repository } from 'typeorm';
+import { CreateInput, CreateOutPut } from './dtos/create.dto';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(
+    @InjectRepository(TravelPost)
+    private readonly travelPostRepository: Repository<TravelPost>,
+  ) {}
+
+  async createPost(createInput: CreateInput): Promise<CreateOutPut> {
+    return this.travelPostRepository.save(
+      this.travelPostRepository.create(createInput),
+    );
+  }
+}
