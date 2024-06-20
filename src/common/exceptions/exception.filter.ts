b4a300856ebd3c83c22e9eca19cd434430bad301
inde153 <inde153@naver.com>
2024-06-20 19:47:06 +0900

    refactor: 예외처리 필터 수정

--- src/common/exceptions/exception.filter.ts
+++ src/common/exceptions/exception.filter.ts
@@ -1,24 +1,17 @@
-import {
-  ExceptionFilter,
-  Catch,
-  ArgumentsHost,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
 import { ServiceException } from './service.exception';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
 @Catch(ServiceException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: ServiceException, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
-    const request = ctx.getRequest<Request>();
     const response = ctx.getResponse<Response>();
     const status = exception.errorCode.status;
 
     response.status(status).json({
       message: exception.message,
-      path: request.url,
+      statusCode: status,
     });
   }
 }
