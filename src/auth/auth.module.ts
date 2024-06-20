714515bd5a24d52884fe8e7f69aafe4648783e63
inde153 <inde153@naver.com>
2024-06-20 15:06:15 +0900

    chore : 폴더,파일 구조 변경

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { GoogleStrategy } from 'src/common/auth/google.strategy';
-import { KakaoStrategy } from 'src/common/auth/kakao.strategy';
+import { GoogleStrategy } from 'src/common/strategies/google.strategy';
+import { KakaoStrategy } from 'src/common/strategies/kakao.strategy';
 import { JwtModule } from 'src/jwt/jwt.module';
 import { UserModule } from 'src/user/user.module';
 
