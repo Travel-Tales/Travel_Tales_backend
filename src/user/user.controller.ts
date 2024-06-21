28967e11e449b35bad34a281c22375977467e98b
inde153 <inde153@naver.com>
2024-06-21 20:13:11 +0900

    feat: 마이 프로필 API 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { Role } from 'src/common/decorators/role.decorator';
+import { RoleGuard } from 'src/common/guards/role.guard';
+import { User } from 'src/entities';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Role(['Google', 'Kakao'])
+  @UseGuards(RoleGuard)
+  @Get('/profile')
+  async getMyProfile(@Req() req): Promise<User> {
+    return this.userService.getUserInfoByEmail(req.user);
+  }
 }
