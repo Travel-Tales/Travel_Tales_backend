cb2be5c8f8605adaeb5a0ddff2995391eb418232
inde153 <inde153@naver.com>
2024-07-08 02:05:11 +0900

    refactor : 게시글에 초대하는 메서드 수정

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,17 +2,22 @@ import { forwardRef, Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TravelPost, UserTravelPost } from 'src/entities';
-import { EventGateway } from 'src/event/event.gateway';
+import {
+  TravelPost,
+  UserTravelPost,
+  InvitationVerification,
+} from 'src/entities';
 import { EventModule } from 'src/event/event.module';
-import { UserModule } from 'src/user/user.module';
 import { MailModule } from 'src/mail/mail.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TravelPost, UserTravelPost]),
+    TypeOrmModule.forFeature([
+      TravelPost,
+      UserTravelPost,
+      InvitationVerification,
+    ]),
     EventModule,
-    UserModule,
     MailModule,
   ],
   controllers: [PostController],
