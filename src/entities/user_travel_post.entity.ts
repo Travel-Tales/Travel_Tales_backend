3ed97099d9c5dabe7e2657caff186922deea8bb5
inde153 <inde153@naver.com>
2024-07-20 23:29:32 +0900

    refactor : 엔티티 수정

--- src/entities/user_travel_post.entity.ts
+++ src/entities/user_travel_post.entity.ts
@@ -1,22 +1,34 @@
-import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
 import { TravelPost } from './travel_post.entity';
 import { User } from './user.entity';
 
 @Entity()
 export class UserTravelPost {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryColumn()
+  userId: number;
+
+  @PrimaryColumn()
+  postId: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
 
   @ManyToOne(() => User, (user) => user.userTravelPost, {
     eager: true,
   })
-  @JoinColumn()
+  @JoinColumn({ name: 'userId' })
   user: User;
 
   @ManyToOne(() => TravelPost, (travelPost) => travelPost.userTravelPost, {
     eager: true,
     onDelete: 'CASCADE',
   })
-  @JoinColumn()
+  @JoinColumn({ name: 'postId' })
   travelPost: TravelPost;
 }
