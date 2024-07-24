4c7eadde526bbf2db3326451e7c932211688fdaf
inde153 <inde153@naver.com>
2024-07-25 01:38:44 +0900

    refactor : DTO 수정 및 서비스 로직 변경

--- src/user/dto/update.profile.dto.ts
+++ src/user/dto/update.profile.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
-import { IsFile } from 'nestjs-form-data';
+import { HasMimeType, IsFile, MaxFileSize } from 'nestjs-form-data';
 import { MyProfileOutputDTO } from './myprofile.dto';
 
 export class UpdateProfileInputDto {
@@ -8,10 +8,6 @@ export class UpdateProfileInputDto {
   @IsOptional()
   @ApiProperty({ description: '유저 닉네임' })
   nickname?: string;
-
-  @IsFile()
-  @IsOptional()
-  file?: Express.Multer.File;
 }
 
 export class UpdateProfileOutputDto extends PartialType(MyProfileOutputDTO) {}
