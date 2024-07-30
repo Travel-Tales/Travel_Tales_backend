771d5c2df2ab8d98fac35d69a39e9d6642d40241
inde153 <inde153@naver.com>
2024-07-30 23:24:44 +0900

    refactor : 유저 UpdateDTO수정

--- src/user/dto/update.profile.dto.ts
+++ src/user/dto/update.profile.dto.ts
@@ -4,10 +4,19 @@ import { HasMimeType, IsFile, MaxFileSize } from 'nestjs-form-data';
 import { MyProfileOutputDTO } from './myprofile.dto';
 
 export class UpdateProfileInputDto {
+  @ApiProperty({ description: '유저 닉네임', required: false })
   @IsString()
   @IsOptional()
-  @ApiProperty({ description: '유저 닉네임' })
   nickname?: string;
+
+  @ApiProperty({
+    description: '프로필 이미지 파일, 선택적 필드',
+    type: 'string',
+    format: 'binary',
+    required: false,
+  })
+  @IsOptional()
+  file?: Express.Multer.File;
 }
 
 export class UpdateProfileOutputDto extends PartialType(MyProfileOutputDTO) {}
